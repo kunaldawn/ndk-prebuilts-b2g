4983ebcba8de6622abf9c7603ddef5fd439ea655
Kunal Dawn <kunal.dawn@gmail.com>
2022-01-16 14:06:18 +0530

    r21d-b2g

---
+++ prebuilt/linux-x86_64/include/libyasm-stdint.h
@@ -0,0 +1,9 @@
+#ifndef _YASM_LIBYASM_STDINT_H
+#define _YASM_LIBYASM_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "yasm 1.3.0"
+/* generated using /home/kunaldawn/ndk-build/prebuilts/clang/host/linux-x86/clang-r365631c2/bin/clang --target=x86_64-linux-gnu -B/home/kunaldawn/ndk-build/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/x86_64-linux/bin --sysroot=/home/kunaldawn/ndk-build/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/sysroot -L/home/kunaldawn/ndk-build/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/lib/gcc/x86_64-linux/4.8.3 -B/home/kunaldawn/ndk-build/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/lib/gcc/x86_64-linux/4.8.3 -L/home/kunaldawn/ndk-build/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/x86_64-linux/lib64 -B/home/kunaldawn/ndk-build/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/x86_64-linux/lib64 -L/home/kunaldawn/ndk-build/prebuilts/clang/host/linux-x86/clang-r365631c2/lib64 -B/home/kunaldawn/ndk-build/prebuilts/clang/host/linux-x86/clang-r365631c2/lib64 -Os -fomit-frame-pointer -w -s */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
